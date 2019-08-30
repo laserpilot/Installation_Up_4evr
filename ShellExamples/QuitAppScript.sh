610c39db8667cfa226bfe039142650f00f14a470
Blair Neal <laser@blairs-mbp.lan>
2019-08-30 18:51:39 -0400

    Adding sample scripts and some more updates

--- ShellExamples/QuitAppScript.sh
+++ ShellExamples/QuitAppScript.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-osascript -e "quit app \"twitterTimeline\""
-#osa script does not appear to work properly with ofxWatchdog - switching to killall
-killall twitterTimeline
-kill `pgrep Curator`
-echo $"Shutting Down Timeline App and Curator"
+killall TweetBot
+kill `pgrep Tweetbot`
+echo "Shutting Down 2 Apps"
